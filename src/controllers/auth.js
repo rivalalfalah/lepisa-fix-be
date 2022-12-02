f75daa08e01c9d39bba5b47d45efee3d19ffdd86
lepisa <emaillepisa@gmail.com>
2022-12-03 00:53:45 +0700

    fix button url

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -42,7 +42,7 @@ const auth = {
         name: req.body.first_name,
         // template: "verificationEmail.html",
         template: "verificationEmail.html",
-        buttonUrl: `https://lepisa-fix-be.vercel.app/auth/${setData.pinActivation}`,
+        buttonUrl: `https://lepisa-fe.vercel.app/auth/${setData.pinActivation}`,
       };
 
       await sendMail(setSendEmail);
