85ada36772535f76a039e7c921435c00007d9bef
lepisa <emaillepisa@gmail.com>
2022-12-04 18:31:57 +0700

    fixx

--- src/repo/booking.js
+++ src/repo/booking.js
@@ -148,6 +148,21 @@ const updateStatusTicket = (id, data) => {
   });
 };
 
+
+const getTiketByBookingId = (req) => {
+  return new Promise((resolve, reject) => {
+    const { booking_id } = req.params;
+    const getQuery =
+      `select movies.tittle,schedule.date,category.name,schedule.price,seat.seat,category_age.name as age,booking.payment_id from booking inner join schedule on booking.schedule_id = schedule.id inner join movies on schedule.movie_id = movies.id inner join category on movies.category_id = category.id inner join category_age on movies.category_age_id = category_age.id inner join booking_seat on booking.id = booking_seat.booking_id inner join seat on booking_seat.seat_id = seat.id where booking.id = $1`
+    db.query(getQuery,[booking_id] ,(error, result) => {
+      if (error) {
+        console.log(error);
+        return reject({ status: 500, msg: "internal server error" });
+      }
+      resolve({ status: 200, data: result.rows });
+    });
+  });
+};
 const bookingRepo = {
   createBooking,
   createBookingSeat,
@@ -157,6 +172,7 @@ const bookingRepo = {
   updatePointUser,
   getTiketByPaymentId,
   updateStatusTicket,
+  getTiketByBookingId
 };
 
 module.exports = bookingRepo;
